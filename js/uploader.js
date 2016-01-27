21abe62a7d5e5c1d34d3b247516a9f7541e45cff
Ivana Setiawan <ivana@funda.nl>
2016-01-27 10:53:53 +0100

    Fieldset checked

--- js/uploader.js
+++ js/uploader.js
@@ -1,7 +1,9 @@
 ; (function (document, window, index) {
     var fileUploader = document.getElementById('fileUploader');
     var preview = document.getElementById('preview');
+    var uploadField = u('.test');
     var uploadLabel = u('[data-upload-label]');
+    var uploadButton = u('[data-upload-button]');
 
     function windowLoadHandler() {
         window.removeEventListener('load', windowLoadHandler);
@@ -17,19 +19,20 @@
             preview.appendChild(image);
             preview.className = 'uploadbox__preview uploaded';
 
+            oFReader.addEventListener('load', function (event) {
+                image.src = event.target.result;
+                this.removeEventListener('load');
+            }, false);
+            oFReader.readAsDataURL(file);
+
             if (preview.classList.contains('uploaded')) {
                 if (u(preview).find('img').length > 1) {
                     u(preview).find('img').nodes[0].remove();
                 }
                 uploadLabel.addClass('reupload');
                 uploadLabel.find('span').html('Reupload');
+                document.getElementById('next').disabled = false;
             }
-
-            oFReader.addEventListener('load', function (event) {
-                image.src = event.target.result;
-                this.removeEventListener('load');
-            }, false);
-            oFReader.readAsDataURL(file);
         });
     }
 

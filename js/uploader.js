93804d62557873441b55c3e1f4b7d09cfbd6c4f0
Ivana Setiawan <ivana@funda.nl>
2016-01-27 14:50:06 +0100

    umbrella to shoestring and update code base

--- js/uploader.js
+++ js/uploader.js
@@ -1,41 +1,72 @@
 ; (function (document, window, index) {
-    var fileUploader = document.getElementById('fileUploader');
-    var preview = document.getElementById('preview');
-    var uploadField = u('.test');
-    var uploadLabel = u('[data-upload-label]');
-    var uploadButton = u('[data-upload-button]');
+    var uploadFields = document.querySelectorAll('[data-upload-field]');
+    var fileUploaderS = document.querySelector('[data-file-uploader]');
+    var uploadLabel = shoestring('[data-upload-label]');
+    var uploadButton = shoestring('[data-upload-button]');
 
     function windowLoadHandler() {
         window.removeEventListener('load', windowLoadHandler);
-        fileUploader.addEventListener('change', PreviewImages, false);
-    }    
-    
-    function PreviewImages() {
-        Array.prototype.forEach.call(fileUploader.files, function (file, index) {
-            var oFReader = new FileReader(),
-                image;
-            image = document.createElement('img');
-            image.id = 'isImg';
-            preview.appendChild(image);
-            preview.className = 'uploadbox__preview uploaded';
+        Array.prototype.forEach.call(uploadFields, function (element, index) {
+            var fileuploader = shoestring(element).find('[data-file-uploader]');
+            shoestring(fileuploader).on('change', PreviewImagesAlt, false);
+        });
+        // fileUploaderS.addEventListener('change', PreviewImages, false);
+    }
 
+    function PreviewImagesAlt() {
+        var preview = shoestring(this).closest('.uploadbox').find('[data-upload-preview]');
+        console.log(shoestring(this));
+        // Array.prototype.forEach.call(fileUploaderS.files, function (file, index) {
+        //     console.log(file);
+        //     var oFReader = new FileReader();
+        //     var image;
+        //     oFReader.addEventListener('load', function (event) {
+        //         image = '<img src="' + event.target.result +'">';
+        //         preview.append(image);
+        //         preview.addClass('uploaded');
+        //         this.removeEventListener('load');
+    
+        //         if (shoestring(preview).attr('class') === 'uploadbox__preview uploaded') {
+        //             if (shoestring(preview).find('img').length > 1) {
+        //                 shoestring(preview).find('img')[0].remove();
+        //             }
+        //             uploadLabel.addClass('reupload');
+        //             uploadLabel.find('span').html('Reupload');
+        //             document.getElementById('next').disabled = false;
+        //         }
+        //     }, false);
+        //     oFReader.readAsDataURL(file);
+        // });
+    }
+
+    function PreviewImages() {
+        var preview = shoestring(this).closest('.uploadbox').find('[data-upload-preview]');
+        console.log(fileUploaderS.files);
+        Array.prototype.forEach.call(fileUploaderS.files, function (file, index) {
+            console.log(file);
+            var oFReader = new FileReader();
+            var image;
             oFReader.addEventListener('load', function (event) {
-                image.src = event.target.result;
+                image = '<img src="' + event.target.result +'">';
+                preview.append(image);
+                preview.addClass('uploaded');
                 this.removeEventListener('load');
+    
+                if (shoestring(preview).attr('class') === 'uploadbox__preview uploaded') {
+                    if (shoestring(preview).find('img').length > 1) {
+                        shoestring(preview).find('img')[0].remove();
+                    }
+                    uploadLabel.addClass('reupload');
+                    uploadLabel.find('span').html('Reupload');
+                    document.getElementById('next').disabled = false;
+                }
             }, false);
             oFReader.readAsDataURL(file);
-
-            if (preview.classList.contains('uploaded')) {
-                if (u(preview).find('img').length > 1) {
-                    u(preview).find('img').nodes[0].remove();
-                }
-                uploadLabel.addClass('reupload');
-                uploadLabel.find('span').html('Reupload');
-                document.getElementById('next').disabled = false;
-            }
         });
     }
 
     window.addEventListener('load', windowLoadHandler, false);
 
 }( document, window, 0 ));
+
+
